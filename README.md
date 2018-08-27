cbe36901434403ba44e2e7fdae8b34f3bb620ffb
justin <justinvyang@gmail.com>
2018-08-27 08:56:19 -0700

    added icons

---
+++ README.md
@@ -0,0 +1,26 @@
+## Project 4 R10
+
+Author: Justin Yang APD student at RED Academy 2018
+
+Front End:
+
+- react-native
+
+Back End:
+
+- react-realm
+
+R10 is a conference app built on React Native using react-realm
+
+- Download the repo
+- Run npm install
+- Download Xcode
+- Run react-native run-ios
+  OR
+  react-native run-android
+
+## Future Improvements
+
+- Add a Map
+- Add custom splash
+- Proptype validtion
--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import { Text, View, StatusBar } from "react-native";
+// import MapView from "react-native-maps";
 
 const Map = ({ mapData }) => {
   return (
     <View>
       <StatusBar barStyle="light-content" />
+      {/* <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      /> */}
       <Text> Map </Text>
     </View>
   );
